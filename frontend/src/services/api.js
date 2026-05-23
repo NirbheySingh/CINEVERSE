0a382f649e9b2b00f69f6c6dc5452d2db12a5b70
Nirbhey Singh <nirbhey07@gmail.com>
2026-05-23 21:43:22 +0530

    Connect backend with frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,43 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: '/api',
+//   withCredentials: true, // Important for cookies (refresh tokens)
+// });
+
+// api.interceptors.request.use((config) => {
+//   const stored = localStorage.getItem('user');
+//   if (stored) {
+//     try {
+//       const user = JSON.parse(stored);
+//       if (user?.accessToken) {
+//         config.headers.Authorization = `Bearer ${user.accessToken}`;
+//       }
+//     } catch {
+//       // ignore invalid localStorage
+//     }
+//   }
+//   return config;
+// });
+
+// // Response Interceptor for handling global errors (like 401s)
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     // If we have a refresh token logic, we'd handle 401 token expiry here.
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
+
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-  withCredentials: true, // Important for cookies (refresh tokens)
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
@@ -20,11 +55,9 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// Response Interceptor for handling global errors (like 401s)
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    // If we have a refresh token logic, we'd handle 401 token expiry here.
     return Promise.reject(error);
   }
 );
